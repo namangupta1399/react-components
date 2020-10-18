d8da65574e1dd786a6f6f190b657de573f9dff2a
namangupta1399 <namangupta1399@gmail.com>
2020-10-18 23:18:57 +0530

    added build

---
+++ precache-manifest.6e887a04dcf5a098fdc3a5efd109ff80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea2669abaa211c563e5bea03f7343a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62ad0831de3bf0e638a8",
+    "url": "/static/css/main.5e14974b.chunk.css"
+  },
+  {
+    "revision": "7e8557ee8e6e2539114a",
+    "url": "/static/js/2.ba955749.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ba955749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ad0831de3bf0e638a8",
+    "url": "/static/js/main.a4571339.chunk.js"
+  },
+  {
+    "revision": "f0e9e0561a1081db1f31",
+    "url": "/static/js/runtime-main.0a541afb.js"
+  }
+]);
